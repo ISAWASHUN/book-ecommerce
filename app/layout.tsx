4197ac229f0acb07217bdcaf31356b25e96439c5
ISAWASHUN <syun20000616@icloud.com>
2023-12-07 23:10:13 +0900

    認証の完了

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Noto_Sans_JP } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
+import { SessionProvider } from 'next-auth/react'
+import { NextAuthProvider } from './lib/next-auth/provider'
 
 const notoSansJP = Noto_Sans_JP({subsets: ["latin"], weight: ["400"]})
 
@@ -18,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={notoSansJP.className}>
+        <NextAuthProvider>
         <Header/>
+        </NextAuthProvider>
         {children}
       </body>
     </html>
--- app/lib/next-auth/options.ts
+++ app/lib/next-auth/options.ts
@@ -1,12 +1,14 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { NextAuthOptions, NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github"
+import prisma from "../prisma";
 
-export const NextAuthOptions: NextAuthOptions = {
+export const nextAuthOptions: NextAuthOptions = {
   debug: false,
   providers: [
     GitHubProvider({
-      cliendId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
   adapter: PrismaAdapter(prisma),
@@ -20,5 +22,6 @@ export const NextAuthOptions: NextAuthOptions = {
         },
       }
     }
-  }
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 }
